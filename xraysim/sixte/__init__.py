6faa662abba783a5f875c66c51ffc9046cdc896a
Mauro Roncarelli <mauro.roncarelli@gmail.com>
2025-06-30 15:18:56 +0200

    Removed the `__classes`, `__methods` and `__shared` attributes from 'sixte' module. Now attributes and method are accessible only directly via the `sixte` module.

--- xraysim/sixte/__init__.py
+++ xraysim/sixte/__init__.py
@@ -1,3 +1,6 @@
-from xraysim.sixte.__methods import *
 from xraysim.sixte.__classes import *
+from xraysim.sixte.__methods import *
 from xraysim.sixte.__shared import *
+del __classes
+del __methods
+del __shared
