2083c49f27482d503f4efdefb276d733dfbc7e22
Mauro Roncarelli <mauro.roncarelli@gmail.com>
2025-11-18 14:21:11 +0100

    Minor refactorings.

--- tests/test_ideal_runs.py
+++ tests/test_ideal_runs.py
@@ -24,18 +24,18 @@ snapshotFile = inputDir + 'snap_Gadget_sample'
 
 # Emission table parameters
 abund = "aspl"  # Xspec abundance table
-nH = 0.02  # [10^22 cm-2]
 nene = 6000
 e_min, e_max = 2., 8.  # Energy range [keV]
 d_ene = (e_max - e_min) / nene  # Energy bin [keV]
 energy = np.linspace(e_min, e_max, num=nene, endpoint=False) + 0.5 * d_ene  # [keV]
 dz = 1e-3
-nSigma = 5.  # Used to define the redshift range in units of the velocity dispersion defined below
+nSigmaVel = 5.  # Used to define the redshift range in units of the velocity dispersion defined below
 
 # Map parameters
 npix = 50
 
 # Setting physical parameters of the tests with a true random generator
+nHMin, nHMax = 0, 0.05
 tMin, tMax = 2, 9
 zMin, zMax = 0, 1.5
 metalMin, metalMax = 0.2, 1
@@ -43,23 +43,26 @@ sigmaVMin, sigmaVMax = 50, 500
 
 TRG = TrueRandomGenerator()
 errMsg = "Random seed: " + str(TRG.initialSeed)  # Assertion error message if test fails
+nH = TRG.uniform(nHMin, nHMax)  # Hydrogen column density [10^22 cm-2]
 temp = TRG.uniform(tMin, tMax)  # Gas temperature [keV]
 z = int(TRG.uniform(zMin, zMax) / dz) * dz  # redshift [---] (multiple of dz so that its values is in the table)
 metal = TRG.uniform(metalMin, metalMax)  # metallicity [Solar]
 sigma_v = TRG.uniform(sigmaVMin, sigmaVMax)  # velocity dispersion [km/s]
 
-
 # Temperature ranges in the table
 nt, tMinTable, tMaxTable = 3, temp / 1.5, temp * 1.5
 
 # Calculating z_min and z_max for the spectral table: they correspond to z ± nSigma * sigma_v, rounded with dz
-zMinTable = np.floor(vpec2zobs(-nSigma * sigma_v, z, 'km/s') / dz) * dz
-zMaxTable = np.ceil(vpec2zobs(nSigma * sigma_v, z, 'km/s') / dz) * dz
+zMinTable = np.floor(vpec2zobs(-nSigmaVel * sigma_v, z, 'km/s') / dz) * dz
+zMaxTable = np.ceil(vpec2zobs(nSigmaVel * sigma_v, z, 'km/s') / dz) * dz
 nz = int(np.round((zMaxTable - zMinTable) / dz)) + 1
 if nz < 3:
     zMinTable -= dz
     zMaxTable += dz
     nz = int(np.round((zMaxTable - zMinTable) / dz)) + 1
+
+# Creating the spectral table
+specTable = apec_table(nz, zMinTable, zMaxTable, nt, tMinTable, tMaxTable, nene, e_min, e_max, metal, abund=abund)
 
 # Calculating normalization
 cosmo = cosmology.FlatLambdaCDM(H0=100., Om0=0.3)
@@ -71,7 +74,9 @@ h_Hubble = pygr.readhead(snapshotFile, 'hubble')
 map_str = make_map(snapshotFile, 'nenH', 1, center=[2500., 2500.], size=mapSize, struct=True, tcut=1e6)
 InenHdl = map_str['map'][0, 0]  # [h^3 cn^-5] (comoving)
 norm = InenHdl * 1e-14 * h_Hubble ** 3 * (1 + z) ** 3 * mapSize ** 2 / (4 * pi * d_C ** 2) # [10^14 cm^-5] (physical)
-# convNorm = 1e-14 * lPix**2 / (4 * np.pi * d_C ** 2) * h_Hubble ** 3 * (1 + zObs) ** 3 # [h^3 cm^-5] --> [10^14 cm^-5]
+
+rightParsV0 = (nH, temp, metal, z, 0, norm)      # Normalization [10^14 cm^-5]
+rightPars = (nH, temp, metal, z, sigma_v, norm)      # Normalization [10^14 cm^-5]
 
 # Test files
 referenceDir = os.environ.get('XRAYSIM') + '/tests/reference_files/'
@@ -114,14 +119,10 @@ def test_isothermal_no_velocities():
     # Computing reference spectrum
     specRef = wabs_bapec(nH, temp, metal, z, 0, norm)  # [photons s^-1 cm^-2] (already multiplied by norm)
 
-    # Creating the spectral table
-    specTable = apec_table(nz, zMinTable, zMaxTable, nt, tMinTable, tMaxTable, nene, e_min, e_max, metal, abund=abund)
 
-    # Creating the speccube from the snapshot assuming isothermal gas with Gaussian velocity distribution
+    # Creating the speccube from the snapshot assuming isothermal gas with no velocities velocity distribution
     specCube = make_speccube(snapshotFile, specTable, XRISM_FOV / 60., npix, z, center=[2500., 2500.], proj='z',
                              tcut=1e6, isothermal=temp * keV2K, nh=nH, novel=True)
-
-    del specTable
 
     # Checking that the integrated spectrum matches with the reference one
     assert specCube['energy'] == pytest.approx(energy, rel=1e-6), errMsg  # [keV]
@@ -165,18 +166,11 @@ def test_isothermal_no_velocities():
 
     # Fitting the spectrum in the pha file with parameters starting with the right values
     specfitRightStart = SpecFit(phaFile, "wabs(bapec)")
-    rightStartPars = (nH,  # nH [10^22 cm^-2]
-                      temp,  # kT [keV]
-                      metal,  # Abundance [Solar]
-                      z,  # Redshift [---]
-                      0.,  # Velocity dispersion [km/s]
-                      norm)      # Normalization [10^14 cm^-5]
 
     fixedPars = (True, False, False, False, True, False)
-    specfitRightStart.run(start=rightStartPars, fixed=fixedPars, method='cstat', abund=abund, erange=(e_min, e_max))
+    specfitRightStart.run(start=rightParsV0, fixed=fixedPars, method='cstat', abund=abund, erange=(e_min, e_max))
 
-    #assert_fit_results_nominal_within_tolerance(specfitRightStart, rightStartPars, tol=1e-3)
-    assert_fit_results_within_error(specfitRightStart, rightStartPars, tol=1.5, msg=errMsg)
+    assert_fit_results_within_error(specfitRightStart, rightParsV0, tol=1.5, msg=errMsg)
     del specfitRightStart
 
     # Fitting the spectrum in the pha file starting with wrong parameters
@@ -185,11 +179,10 @@ def test_isothermal_no_velocities():
                  TRG.uniform(low=tMin, high=tMax),  # kT [keV]
                  TRG.uniform(low=metalMin, high=metalMax),  # Abundance [Solar]
                  z,  # Redshift [---]
-                 0.,  # Velocity dispersion [km/s]
-                 1.)                                    # Normalization [10^14 cm^-5]
+                 0,  # Velocity dispersion [km/s]
+                 1)                                    # Normalization [10^14 cm^-5]
 
     fixedPars = (True, False, False, False, True, False)
     specfitWrongStart.run(start=startPars, fixed=fixedPars, method='cstat', abund=abund, erange=(e_min, e_max))
 
-    assert_fit_results_within_error(specfitWrongStart, (nH, temp, metal, z, sigma_v,
-                                                        norm), tol=3, msg=errMsg)
+    assert_fit_results_within_error(specfitWrongStart, rightParsV0, tol=4, msg=errMsg)
